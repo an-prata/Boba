05fce4ed41aa37bc98ba0a29292f027d426ed8a7
an-prata <evanrileyoverman@gmail.com>
2021-10-24 17:12:07 -0700

    Improved PasswordManager.FileHandling.XmlHandler and added unit tests for PasswordManager.FileHandling.XmlHandler and PasswordManager.FileHandling.JsonHandler

--- Boba.PasswordManager/FileHandling/XmlHandler.cs
+++ Boba.PasswordManager/FileHandling/XmlHandler.cs
@@ -1,8 +1,4 @@
-﻿// Boba Password Manager (https://github.com/an-prata/Boba)
-// Copyright (c) 2021 Evan Overman (https://github.com/an-prata)
-// Licensed under the MIT License.
-
-using System;
+﻿using System;
 using System.IO;
 using System.Xml.Serialization;
 using System.Security.Cryptography;
@@ -14,15 +10,15 @@ namespace Boba.PasswordManager.FileHandling
 		public static void SaveToFile(string filePath, object value)
 		{
 			using FileStream fileStream = new FileStream(filePath, FileMode.Create);
-			XmlSerializer xmlSerializer = new XmlSerializer(typeof(RSAParameters));
+			XmlSerializer xmlSerializer = new XmlSerializer(value.GetType());
 			xmlSerializer.Serialize(fileStream, value);
 		}
 
-		public static RSAParameters ReadFromFile(string filePath)
+		public static TValue ReadFromFile<TValue>(string filePath)
 		{
 			using FileStream fileStream = new FileStream(filePath, FileMode.Open);
-			XmlSerializer xmlSerializer = new XmlSerializer(typeof(RSAParameters));
-			return (RSAParameters)xmlSerializer.Deserialize(fileStream);
+			XmlSerializer xmlSerializer = new XmlSerializer(typeof(TValue));
+			return (TValue)xmlSerializer.Deserialize(fileStream);
 		}
 	}
 }
---
+++ Boba.PasswordManagerTests/JsonHandlerUnitTests.cs
@@ -0,0 +1,27 @@
+﻿// Boba Password Manager (https://github.com/an-prata/Boba)
+// Copyright (c) 2021 Evan Overman (https://github.com/an-prata)
+// Licensed under the MIT License.
+
+using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Boba.PasswordManager;
+using Boba.PasswordManager.FileHandling;
+
+namespace Boba.PasswordManagerTests
+{
+    [TestClass]
+    public class JsonHandlerUnitTests
+    {
+		private readonly byte[] TestBytes = Encoding.UTF8.GetBytes("hd82bdciw08723bisd83b");
+
+		[TestMethod]
+		public void SaveAndReadTest()
+		{
+			PasswordEntry passwordEntry = new PasswordEntry(TestBytes, "Google.com", "Jake@gmail.com");
+			JsonHandler.SaveToFile("passwordentry.xml", passwordEntry);
+			Assert.AreEqual(passwordEntry.Username, JsonHandler.ReadFromFile<PasswordEntry>("passwordentry.xml").Username);
+		}
+	}
+}
